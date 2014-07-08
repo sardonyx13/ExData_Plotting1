c0e93ca5451c1fabca423cfbcec999986137c649
Alex <solomin@gmail.com>
2014-07-08 16:56:46 +0400

    plot1 completed

--- plot1.R
+++ plot1.R
@@ -1,2 +1,11 @@
 
+source('readDataForPlot.R')
 
+df <- readDataForPlot()
+
+png("plot1.png", width = 480, height = 480)
+
+hist(df$Global_active_power, col="red", main = "Global Active Power", 
+     xlab = "Global Active Power (kilowatts)")
+
+dev.off()
