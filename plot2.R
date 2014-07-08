1db434fdece09fed595904c4c0281ae1753d6e8a
Alex <solomin@gmail.com>
2014-07-08 17:01:31 +0400

    initial checkin

---
+++ plot2.R
@@ -0,0 +1,9 @@
+# a script readDataForPlot.R contains function 'readDataForPlot' that reads 
+# and returns data.frame from household_power_consumption.txt
+#
+# the result data.frame contains rows for dates: 2007-02-01 and 2007-02-02 only
+#
+# see 'readDataForPlot.R' for details of implementation
+source('readDataForPlot.R')
+
+df <- readDataForPlot()
